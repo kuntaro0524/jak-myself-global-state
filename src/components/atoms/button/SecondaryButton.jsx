e7224cd10acd21f3628824ed2bf8ba3e5488c5d6
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-11-02 10:47:17 +0900

    ひとまずの準備 onClick関数をpropsでSecondaryButtonにわたして、管理者とユーザのボタンを押したときに、Alertを出して確認する

--- src/components/atoms/button/SecondaryButton.jsx
+++ src/components/atoms/button/SecondaryButton.jsx
@@ -1,8 +1,10 @@
 import styled from "styled-components";
 
 export const SecondaryButton = (props) => {
-  const { children } = props;
-  return <SButton>{children}</SButton>;
+  // propsとしてボタンを押したときの関数を受け取って
+  const { children, onClick } = props;
+  // ボタンを押したときの関数として割り当てる
+  return <SButton onClick={onClick}>{children}</SButton>;
 };
 
 const SButton = styled.button`
@@ -10,8 +12,10 @@ const SButton = styled.button`
   color: #fff;
   padding: 6px 24px;
   border: none;
-  border-radius: 2px;
+  border-radius: 15px;
   outline: none;
+  font-size: 18px;
+  width: 300px;
   &:hover {
     cursor: pointer;
     opacity: 0.8;
--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,9 +1,17 @@
 import styled from "styled-components";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
 
 export const Top = () => {
+  const onClickAdmin = () => alert("管理者だよ");
+  const onClickUser = () => alert("ユーザだよ");
   return (
     <SContainer>
       <h2> Top page death! </h2>
+      <SecondaryButton onClick={onClickAdmin}> 管理者ユーザ </SecondaryButton>
+      <br />
+      <br />
+
+      <SecondaryButton onClick={onClickUser}> 一般ユーザ </SecondaryButton>
     </SContainer>
   );
 };
